ca5fe6143c7eb0bd3a3b42faad81ebed52b4267b
Tai Phung <taiphungdinh@gmail.com>
2020-03-22 19:56:42 +0700

    Initial project
    					2020/03/22
    
    DESCRIPTION
    
      Applicatio run successfully with functions:
      - Open single image file
      - Auto scroll
      - Picture box with size mode AutoSize for first load
      - Mouse wheel event on picture box for zooming in/out
      - Zoom scale 1/8 -> 8

---
+++ ImageViewer/PhotoViewer.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ImageViewer
+{
+    partial class PhotoViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhotoViewer));
+            this.mnsMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlViewer = new System.Windows.Forms.Panel();
+            this.ptbViewer = new System.Windows.Forms.PictureBox();
+            this.mnsMain.SuspendLayout();
+            this.pnlViewer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnsMain
+            // 
+            this.mnsMain.BackColor = System.Drawing.SystemColors.Control;
+            this.mnsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.mnsMain.Location = new System.Drawing.Point(0, 0);
+            this.mnsMain.Name = "mnsMain";
+            this.mnsMain.Size = new System.Drawing.Size(520, 24);
+            this.mnsMain.TabIndex = 3;
+            this.mnsMain.Text = "Main Menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // pnlViewer
+            // 
+            this.pnlViewer.AutoScroll = true;
+            this.pnlViewer.AutoSize = true;
+            this.pnlViewer.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlViewer.Controls.Add(this.ptbViewer);
+            this.pnlViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlViewer.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.pnlViewer.Location = new System.Drawing.Point(0, 24);
+            this.pnlViewer.Name = "pnlViewer";
+            this.pnlViewer.Size = new System.Drawing.Size(520, 489);
+            this.pnlViewer.TabIndex = 4;
+            // 
+            // ptbViewer
+            // 
+            this.ptbViewer.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ptbViewer.Location = new System.Drawing.Point(4, 4);
+            this.ptbViewer.Name = "ptbViewer";
+            this.ptbViewer.Size = new System.Drawing.Size(512, 482);
+            this.ptbViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbViewer.TabIndex = 0;
+            this.ptbViewer.TabStop = false;
+            // 
+            // PhotoViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 513);
+            this.Controls.Add(this.pnlViewer);
+            this.Controls.Add(this.mnsMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PhotoViewer";
+            this.Text = "Simple Photo Viewer by Tai Phung";
+            this.Load += new System.EventHandler(this.PhotoViewer_Load);
+            this.mnsMain.ResumeLayout(false);
+            this.mnsMain.PerformLayout();
+            this.pnlViewer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnsMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlViewer;
+        private System.Windows.Forms.PictureBox ptbViewer;
+    }
+}
+
---
+++ ImageViewer/PhotoViewer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class PhotoViewer : Form
+    {
+        private double _scaleRatio;
+        public PhotoViewer()
+        {
+            _scaleRatio = 1.0;
+
+            InitializeComponent();
+        }
+
+        private void PhotoViewer_Load(object sender, EventArgs e)
+        {
+            this.ptbViewer.MouseWheel += this.ptbViewer_MouseWheel;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png, *.jpeg, *.jpg, *.bmp)|*.png;*.jpg;*.jpeg;*.bmp|PNG (*.png)|*.png|JPEG (*.jpeg)|*.jpeg|JPG (*.jpg)|*.jpg|BMP (*.bmp)|*.bmp";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ptbViewer.Image = Image.FromFile(openFileDialog.FileName);
+                ptbViewer.Width = ptbViewer.Image.Width;
+                ptbViewer.Height = ptbViewer.Image.Height;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ptbViewer_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (_scaleRatio >= 0.125 && _scaleRatio <= 4)
+            {
+                if (e.Delta > 0)
+                {
+                    if (_scaleRatio < 4)
+                    {
+                        _scaleRatio *= 2;
+                        Zoom();
+                    }
+                }
+                else
+                {
+                    if (_scaleRatio > 0.125)
+                    {
+                        _scaleRatio /= 2;
+                        Zoom();
+                    }
+                }
+            }
+        }
+
+        private void Zoom() {
+            ptbViewer.Width = (int)(ptbViewer.Image.Width * _scaleRatio);
+            ptbViewer.Height = (int)(ptbViewer.Image.Height * _scaleRatio);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Developed by Tai Phung\nEmail: taiphungdinh@gmail.com";
+            MessageBox.Show(message, "About", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+        }
+    }
+}
